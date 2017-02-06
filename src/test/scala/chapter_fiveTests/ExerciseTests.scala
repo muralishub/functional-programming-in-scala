96e19ed18131251aec1444916b4e0c73b346c3b5
devboxnewchurch <devboxnewchurch@bigbird>
2017-02-06 14:01:20 +0000

    chapter 5

--- src/test/scala/chapter_fiveTests/ExerciseTests.scala
+++ src/test/scala/chapter_fiveTests/ExerciseTests.scala
@@ -10,7 +10,7 @@ class ExerciseTests extends FunSpec{
 val e = new Exercises
     e.if2(false, sys.error(""), 3) shouldBe 3
 
-    def maybeTwice(b: Boolean, i: => Int) = if (b) i+i else 0
+    def maybeTwice(b: Boolean, i: => Int) = {if (b) i+i else 0}
     def maybeTwice2(b: Boolean, i: => Int) = {
       lazy val j = i
       if (b) j+j else 0
